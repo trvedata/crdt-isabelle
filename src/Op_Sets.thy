d21add35697e68876277d0efccdb813edcaddec8
Martin Kleppmann <martin@kleppmann.com>
2017-06-25 22:25:13 +0100

    Recurse up ancestor chain in search of next list item

--- src/Op_Sets.thy
+++ src/Op_Sets.thy
@@ -40,18 +40,27 @@ definition next_elem_sibling :: "('objId, 'elemId::linorder) operation set \<Rig
      (\<nexists>child. first_child opers listId (Some prevId) child) \<and>
      next_sibling opers listId prevId nextId"
 
-(* TODO aunt needs to search (grand)+parents' siblings as well *)
-definition next_elem_aunt :: "('objId, 'elemId::linorder) operation set \<Rightarrow> 'objId \<Rightarrow> 'elemId \<Rightarrow> 'elemId \<Rightarrow> bool" where
-  "next_elem_aunt opers listId prevId nextId \<equiv>
+inductive siblingless_ancestor :: "('objId, 'elemId::linorder) operation set \<Rightarrow> 'objId \<Rightarrow> 'elemId \<Rightarrow> 'elemId \<Rightarrow> bool" where
+  "\<lbrakk> \<nexists>sibling. next_sibling opers listId startId sibling \<rbrakk>
+     \<Longrightarrow> siblingless_ancestor opers listId startId startId" |
+  "\<lbrakk> siblingless_ancestor opers listId startId prevId;
+     child opers listId (Some nextId) prevId;
+     \<nexists>sibling. next_sibling opers listId nextId sibling
+   \<rbrakk> \<Longrightarrow> siblingless_ancestor opers listId startId nextId"
+
+definition next_elem_auntie :: "('objId, 'elemId::linorder) operation set \<Rightarrow> 'objId \<Rightarrow> 'elemId \<Rightarrow> 'elemId \<Rightarrow> bool" where
+  "next_elem_auntie opers listId prevId nextId \<equiv>
      (\<nexists>child. first_child opers listId (Some prevId) child) \<and>
      (\<nexists>sibling. next_sibling opers listId prevId sibling) \<and>
-     (\<exists>parent. child opers listId (Some parent) prevId \<and> next_sibling opers listId parent nextId)"
+     (\<exists>ancestor parent. siblingless_ancestor opers listId prevId ancestor \<and>
+      child opers listId (Some parent) ancestor \<and>
+      next_sibling opers listId parent nextId)"
 
 definition next_elem :: "('objId, 'elemId::linorder) operation set \<Rightarrow> 'objId \<Rightarrow> 'elemId \<Rightarrow> 'elemId \<Rightarrow> bool" where
   "next_elem opers listId prevId nextId \<equiv>
      first_child opers listId (Some prevId) nextId \<or>
      next_elem_sibling opers listId prevId nextId \<or>
-     next_elem_aunt opers listId prevId nextId"
+     next_elem_auntie opers listId prevId nextId"
 
 inductive following_elem :: "('objId, 'elemId::linorder) operation set \<Rightarrow> 'objId \<Rightarrow> 'elemId \<Rightarrow> 'elemId \<Rightarrow> bool" where
   "\<lbrakk> have_list_elem opers listId someId \<rbrakk> \<Longrightarrow> following_elem opers listId someId someId" |
@@ -83,28 +92,15 @@ by(simp add: Collect_mono first_child_def)
 lemma next_elem_exclusive_child:
   assumes "{nextId. first_child opers listId (Some prevId) nextId} \<noteq> {}"
   shows "{nextId. next_elem_sibling opers listId prevId nextId} = {}"
-    and "{nextId. next_elem_aunt opers listId prevId nextId} = {}"
+    and "{nextId. next_elem_auntie opers listId prevId nextId} = {}"
 using assms apply(simp add: next_elem_sibling_def)
-using assms next_elem_aunt_def by force
+using assms next_elem_auntie_def by force
 
 lemma next_elem_exclusive_sibling:
   assumes "{nextId. next_elem_sibling opers listId prevId nextId} \<noteq> {}"
   shows "{nextId. first_child opers listId (Some prevId) nextId} = {}"
-    and "{nextId. next_elem_aunt opers listId prevId nextId} = {}"
-using assms by (simp add: next_elem_aunt_def next_elem_sibling_def)+
-
-lemma set_pick_2:
-  assumes "card S > 1"
-  shows "\<exists>x y. x \<noteq> y \<and> {x, y} \<subseteq> S"
-  using assms
-  apply(subgoal_tac "\<exists>x. x \<in> S") defer
-  using card_gt_0_iff apply fastforce
-  apply(erule exE, rule_tac x=x in exI)
-  apply(subgoal_tac "card (S - {x}) > 0")
-  apply(metis Diff_eq_empty_iff Diff_idemp card_gt_0_iff insertCI insert_subsetI subsetI)
-  apply(metis One_nat_def card.insert_remove card_ge_0_finite gr_zeroI insert_Diff
-    insert_Diff_single less_trans nat_neq_iff)
-done
+    and "{nextId. next_elem_auntie opers listId prevId nextId} = {}"
+using assms by(simp_all add: next_elem_auntie_def next_elem_sibling_def)
 
 lemma elem_id_unique:
   assumes "valid_ops opers"
@@ -118,25 +114,6 @@ lemma elem_id_unique:
   apply(subgoal_tac "newOpers = insert oper opers") prefer 2 apply simp
   apply(case_tac "have_list_elem opers listId elemId")
   apply(metis (no_types, lifting) insert_iff valid_op.simps(2))
-  apply(subgoal_tac "{prev. Insert listId prev elemId \<in> opers} = {}")
-  prefer 2 apply (simp add: have_list_elem_def)
-  apply(subgoal_tac "\<exists>prev. oper = Insert listId prev elemId")
-  prefer 2 apply(metis have_list_elem_def insertE, erule exE)
-  apply(subgoal_tac "{prev. Insert listId prev elemId \<in> newOpers} = {prev}")
-  apply simp_all
-done
-
-lemma elem_id_card:
-  assumes "valid_ops opers"
-    and "have_list_elem opers listId elemId"
-  shows "card {prev. Insert listId prev elemId \<in> opers} = 1"
-  using assms apply(induction rule: valid_ops.induct)
-  apply(simp add: have_list_elem_def)
-  apply(subgoal_tac "valid_op opers oper") prefer 2
-  using apply_op.elims apply fastforce
-  apply(subgoal_tac "newOpers = insert oper opers") prefer 2 apply simp
-  apply(case_tac "have_list_elem opers listId elemId")
-  apply(metis (no_types, lifting) Collect_cong insert_iff valid_op.simps(2))
   apply(subgoal_tac "{prev. Insert listId prev elemId \<in> opers} = {}")
   prefer 2 apply (simp add: have_list_elem_def)
   apply(subgoal_tac "\<exists>prev. oper = Insert listId prev elemId")
@@ -161,12 +138,10 @@ lemma parent_exists:
 done
 
 lemma first_child_unique:
-  shows "card {childId. first_child opers listId parentId childId} \<le> 1"
-  apply(subgoal_tac "card {childId. first_child opers listId parentId childId} > 1 \<Longrightarrow> False", force)
-  apply(subgoal_tac "\<exists>x y. x \<noteq> y \<and> {x, y} \<subseteq> {childId. first_child opers listId parentId childId}")
-  apply(metis first_child_def insert_subset mem_Collect_eq not_less_iff_gr_or_eq)
-  using set_pick_2 apply blast
-done
+  assumes "first_child opers listId parentId child1"
+  assumes "first_child opers listId parentId child2"
+  shows "child1 = child2"
+by (meson assms first_child_def not_less_iff_gr_or_eq)
 
 lemma list_invariant:
   assumes "valid_ops opers"
@@ -225,50 +200,51 @@ lemma first_elem_exists:
 done
 
 lemma next_sibling_unique:
-  shows "card {nextId. next_sibling opers listId prevId nextId} \<le> 1"
-  apply(subgoal_tac "card {nextId. next_sibling opers listId prevId nextId} > 1 \<Longrightarrow> False", force)
-  apply(subgoal_tac "\<exists>x y. x \<noteq> y \<and> {x, y} \<subseteq> {nextId. next_sibling opers listId prevId nextId}")
-  apply(metis next_sibling_def insert_subset mem_Collect_eq not_less_iff_gr_or_eq)
-  using set_pick_2 apply blast
-done
+  assumes "next_sibling opers listId prevId next1"
+  assumes "next_sibling opers listId prevId next2"
+  shows "next1 = next2"
+by (meson assms next_sibling_def not_less_iff_gr_or_eq)
 
-lemma next_elem_aunt_unique:
+lemma siblingless_ancestor_indeed:
+  assumes "siblingless_ancestor opers listId startId anc"
+  shows "\<nexists>sibling. next_sibling opers listId anc sibling"
+using assms by(induction rule: siblingless_ancestor.induct, auto)
+
+lemma ancestor_chain:
   assumes "valid_ops opers"
-    and "have_list_elem opers listId prevId"
-  shows "card {nextId. next_elem_aunt opers listId prevId nextId} \<le> 1"
-  apply(case_tac "{nextId. first_child opers listId (Some prevId) nextId} \<noteq> {}")
-  apply(simp add: next_elem_exclusive_child)
-  apply(case_tac "{nextId. next_sibling opers listId prevId nextId} \<noteq> {}")
-  apply(simp add: next_elem_aunt_def)
-  apply(case_tac "\<exists>parentId. child opers listId (Some parentId) prevId", erule exE)
-  prefer 2 apply(simp add: next_elem_aunt_def)
-  apply(subgoal_tac "\<And>p. Insert listId p prevId \<in> opers \<Longrightarrow> p = Some parentId")
-  prefer 2 apply(metis assms(1) child_def elem_id_unique)
-  apply(subgoal_tac "card {nextId. next_elem_aunt opers listId prevId nextId} > 1 \<Longrightarrow> False", force)
-  apply(subgoal_tac "\<exists>x y. x \<noteq> y \<and> {x, y} \<subseteq> {nextId. next_elem_aunt opers listId prevId nextId}")
-  prefer 2 using set_pick_2 apply blast
-  apply((erule exE)+, erule conjE)
-  apply(simp add: next_elem_aunt_def, erule conjE, (erule exE)+, (erule conjE)+)
-  apply(subgoal_tac "parent = parentId \<and> parenta = parentId")
-  prefer 2 apply(metis child_def option.inject, clarsimp)
-  apply(meson next_sibling_def not_less_iff_gr_or_eq)
-done
+    and "ancs = {anc. siblingless_ancestor opers listId startId anc}"
+  shows "\<exists>list. set list = ancs \<and>
+           (\<forall>pre x y suf. list = pre@x#y#suf \<longrightarrow> child opers listId (Some x) y)"
+  using assms
+  apply(induction rule: siblingless_ancestor.inducts)
+  oops
+
+thm siblingless_ancestor.induct
+
+lemma next_elem_auntie_unique:
+  assumes "valid_ops opers"
+    and "next_elem_auntie opers listId prevId next1"
+    and "next_elem_auntie opers listId prevId next2"
+  shows "next1 = next2"
+  using assms apply(simp add: next_elem_auntie_def)
+  apply(erule exE, (erule conjE)+)+
+  apply(case_tac "ancestora = ancestor", clarsimp)
+  apply(metis child_def elem_id_unique next_sibling_unique option.sel)
+  apply(induction rule: siblingless_ancestor.induct)
+  sorry
 
 lemma next_elem_unique:
   assumes "valid_ops opers"
-  shows "card {nextId. next_elem opers listId prevId nextId} \<le> 1"
-  apply(subgoal_tac "{nextId. next_elem opers listId prevId nextId} =
-     {nextId. first_child opers listId (Some prevId) nextId} \<union>
-     {nextId. next_elem_sibling opers listId prevId nextId} \<union>
-     {nextId. next_elem_aunt opers listId prevId nextId}")
-  prefer 2 using next_elem_def apply fastforce
+    and "next_elem opers listId prevId next1"
+    and "next_elem opers listId prevId next2"
+  shows "next1 = next2"
+  using assms apply -
   apply(case_tac "{nextId. first_child opers listId (Some prevId) nextId} \<noteq> {}")
-  apply(metis next_elem_exclusive_child first_child_unique Un_empty_right)
+  apply(metis bex_empty first_child_unique mem_Collect_eq next_elem_def next_elem_exclusive_child)
   apply(case_tac "{nextId. next_elem_sibling opers listId prevId nextId} \<noteq> {}")
-  apply(metis (no_types, lifting) next_elem_exclusive_sibling next_sibling_unique Collect_cong
-    Un_absorb1 empty_def empty_subsetI next_elem_sibling_def sup_bot.right_neutral)
-  apply(metis assms next_elem_aunt_unique Collect_empty_eq One_nat_def card_eq_0_iff child_def
-    have_list_elem_def inf_sup_aci(5) le_SucI le_zero_eq next_elem_aunt_def sup_bot.right_neutral)
+  using next_elem_def next_elem_exclusive_sibling(2) next_elem_sibling_def
+    next_sibling_unique apply fastforce
+  apply(simp add: next_elem_auntie_unique next_elem_def)
 done
 
 thm following_elem.inducts
@@ -286,7 +262,7 @@ lemma following_elem_unaffected:
          \<exists>prev elemId. oper = Insert listId prev elemId")
   using assms apply force
   apply(case_tac "following_elem opers listId firstId elemId")
-  sorry
+  oops
 
 lemma list_order_complete:
   assumes "valid_ops opers"
